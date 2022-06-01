264cf9a85d253cf5efeebac1ff2425703497dbfd
Marcos Saavedra <saavedramarcosdavid@gmail.com>
2022-06-01 11:37:59 -0300

    Guardado y cargado del arbol en un archivo de texto

--- main.c
+++ main.c
@@ -2,32 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXIMA_LONGITUD_CADENA 100
+#define MAXIMA_LONGITUD_CADENA 255
 
 typedef char String[MAXIMA_LONGITUD_CADENA];
 
 struct nodo{
-    // Dato
-    String cadena;
-    // Ramas
-    struct nodo *izq;
-    struct nodo *der;
+	// Dato
+	String cadena;
+	// Ramas
+	struct nodo *izq;
+	struct nodo *der;
 };
 
-typedef struct nodo arbol;
+typedef struct nodo * arbol;
 
 struct nodo * nuevo_nodo(String cadena){
-    // Solicitar memoria
-    size_t tamanioNodo = sizeof(struct nodo);
-    struct nodo *nodo = (struct nodo *)malloc(tamanioNodo);
-    // Asignar el dato e iniciar hojas
-    strcpy(nodo->cadena, cadena);
-    nodo->izq = nodo->der = NULL;
-    return nodo;
+	// Solicitar memoria
+	size_t tamanioNodo = sizeof(struct nodo);
+	struct nodo *nodo = (struct nodo *)malloc(tamanioNodo);
+	// Asignar el dato e iniciar hojas
+	strcpy(nodo->cadena, cadena);
+	nodo->izq = nodo->der = NULL;
+	return nodo;
 }
 
-arbol * inicializa(){
-	arbol * a = nuevo_nodo("Gorrion");
+arbol inicializa(){
+	// arbol a = nuevo_nodo("Gorrion");
+	arbol a = NULL;
 	return a;
 }
 
@@ -38,13 +39,13 @@ void cartel(String nombre_archivo){
 	if (archivo==NULL){
 		printf("Error al abrir el archivo %s", nombre_archivo);
 	}else{
-        while (!feof(archivo)){
-        	fgets(buffer,MAXIMA_LONGITUD_CADENA,archivo);
-            printf("%s", buffer);
-        }
-        fclose(archivo);
-    }
-    fflush(stdin); getchar();
+		while (!feof(archivo)){
+			fgets(buffer,MAXIMA_LONGITUD_CADENA,archivo);
+			printf("%s", buffer);
+		}
+		fclose(archivo);
+	}
+	fflush(stdin); getchar();
 }
 
 void clrscr(){
@@ -68,33 +69,37 @@ void espacio(int n){
 	}
 }
 
-void agregar_arbol(arbol * a){
-    String nuevo,actual,pregunta,res;
-    espacio(2);
-    printf("Que animal es?:  ");
-    fflush(stdin); gets(nuevo);
-    strcpy(actual, a->cadena);
-    espacio(2);
-    printf("Que lo diferencia del/de la %s?  ",actual);
-    fflush(stdin); gets(pregunta);
-    espacio(2);
-    printf("En ese caso, el/la %s %s?  ", nuevo, pregunta);
-    fflush(stdin); gets(res);
-    strcpy(a->cadena, pregunta);
-    if ((!strcmp(res, "Si"))||(!strcmp(res, "SI"))||(!strcmp(res, "si"))){
+void agregar_arbol(arbol a){
+	static int cantidad = 0;
+	String nuevo,actual,pregunta,res;
+	espacio(2);
+	printf("Que animal es?:  ");
+	fflush(stdin); gets(nuevo);
+	strcpy(actual, a->cadena);
+	espacio(2);
+	printf("Que lo diferencia del/de la %s?  ",actual);
+	fflush(stdin); gets(pregunta);
+	espacio(2);
+	printf("En ese caso, el/la %s %s?  ", nuevo, pregunta);
+	fflush(stdin); gets(res);
+	strcpy(a->cadena, pregunta);
+
+	if ((!strcmp(res, "Si"))||(!strcmp(res, "SI"))||(!strcmp(res, "si"))){
 		a->der = nuevo_nodo(nuevo);
 		a->izq = nuevo_nodo(actual);
-    }else{
+		cantidad++;
+	}else{
 		if ((!strcmp(res, "No"))||(!strcmp(res, "NO"))||(!strcmp(res, "no"))){
 			a->izq = nuevo_nodo(nuevo);
 			a->der = nuevo_nodo(actual);
+			cantidad++;
 		}else{
 			printf("Ingrese datos correctos\n");
 		}
-    }
+	}
 }
 
-void adivina(arbol * a, String opc){
+void adivina(arbol a, String opc){
 	if (a!=NULL){
 		if ((a->der==NULL)&&(a->izq==NULL)){
 			espacio(2);
@@ -121,21 +126,57 @@ void adivina(arbol * a, String opc){
 	}
 }
 
-void eliminar_arbol(arbol ** a){
-    if (a != NULL){
-        eliminar_arbol(&(*a)->izq);
-        eliminar_arbol(&(*a)->der);
-        free(a);
-    }
+void eliminar_arbol(arbol * a){
+	if (a != NULL){
+		eliminar_arbol(&(*a)->izq);
+		eliminar_arbol(&(*a)->der);
+		free(a);
+	}
 }
 
-void guardar_arbol(arbol * a){
+void guardar_arbol(arbol a, FILE * archivo){
+	if (a != NULL){
+		fprintf(archivo, "%s\n", a->cadena);
+		guardar_arbol(a->izq, archivo);
+		guardar_arbol(a->der, archivo);
+	}else{
+		fprintf(archivo, "%s\n", "NULL");
+	}
+}
 
+void leer_arbol(arbol * a, FILE * archivo){
+	String cadena;
+	fgets(cadena, MAXIMA_LONGITUD_CADENA, archivo);
+	if (!feof(archivo)){
+		cadena[strlen(cadena)-1] = '\0';
+		// printf("leido: %s\n", cadena);
+		if(strcmp(cadena, "NULL")){
+			*a = nuevo_nodo(cadena);
+			leer_arbol(&((*a)->izq), archivo);
+			leer_arbol(&((*a)->der), archivo);
+		}
+	}
+}
+
+void imprimir_arbol(arbol a){
+	if (a != NULL){
+		printf("%s, ", a->cadena);
+		imprimir_arbol(a->izq);
+		imprimir_arbol(a->der);
+	}
 }
 
 int main(int argc, char const *argv[]){
 	String opc;
-	arbol * a = inicializa();
+	FILE * archivo;
+	arbol a = inicializa();
+
+	archivo = fopen("arbol.bin", "rb");
+	if (archivo != NULL){
+		leer_arbol(&a, archivo);
+		fclose(archivo);
+	}
+
 	inicio();
 	espacio(5);
 	printf("Estas pensando en un animal? ");
@@ -149,9 +190,13 @@ int main(int argc, char const *argv[]){
 		scanf("%s", opc);
 	}
 
-//	guardar_arbol(a);
+	archivo = fopen("arbol.bin", "wb");
+	if (archivo != NULL){
+		guardar_arbol(a, archivo);
+		fclose(archivo);
+	}
 
-//	eliminar_arbol(&a);
+	eliminar_arbol(&a);
 
 	return 0;
 }
